d878129fffe5d034053802b449657ccb8d692fd5
mokkel <61493027+mokkel@users.noreply.github.com>
2022-03-22 17:46:08 +0100

    .

--- script.js
+++ script.js
@@ -4,20 +4,4 @@ document.addEventListener("DOMContentLoaded", start);
 /* når hjemmesiden er loadet, "aktivereres" funktionen "start"*/
 /* Herunder sørger man for, at man kan trykke på alle knapper og filtrerer */
 function start() {
-  const hamburger = document.querySelector(".hamburger");
-  const navMenu = document.querySelector(".nav-menu");
-  const navLink = document.querySelectorAll(".nav-link");
-
-  hamburger.addEventListener("click", mobileMenu);
-  navLink.forEach((n) => n.addEventListener("click", closeMenu));
-
-  function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-  }
-
-  function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-  }
 }
